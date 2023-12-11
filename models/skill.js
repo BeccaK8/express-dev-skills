0fe3e7d2dd2f11b3923d93033a0314e1512b5b6b
Rebecca Kiefer <rebeccakiefer8@gmail.com>
2023-12-11 11:04:23 -0600

    define skills index view and skill model

---
+++ models/skill.js
@@ -0,0 +1,20 @@
+const skills = [
+    {id: 1001, skill: 'JavaScript', level: 'Intermediate' },
+    {id: 1002, skill: 'HTML', level: 'Advanced' },
+    {id: 1003, skill: 'CSS', level: 'Advanced' },
+    {id: 1004, skill: 'Express', level: 'Beginner' },
+];
+
+module.exports = {
+    getAll,
+    getOne
+};
+
+function getAll() {
+    return skills;
+}
+
+function getOne(id) {
+    id = parseInt(id);
+    return skills.find(skill => skills.id === id);
+}
